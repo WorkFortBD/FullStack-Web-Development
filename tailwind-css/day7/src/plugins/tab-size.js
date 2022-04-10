cb96122931fc3f4907558e45d82781ed1e49dfd2
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-04-10 20:04:10 +0600

    Tailwind CSS Day - 7

---
+++ tailwind-css/day7/src/plugins/tab-size.js
@@ -0,0 +1,22 @@
+const plugin = require('tailwindcss/plugin')
+
+module.exports = plugin(function ({ matchUtilities, theme }) {
+    matchUtilities(
+        {
+            tab: (value) => ({
+                tabSize: value
+            }),
+        },
+        { values: theme('tabSize') }
+    )
+}, {
+    theme: {
+        tabSize: {
+            1: '1',
+            2: '2',
+            4: '4',
+            8: '8',
+            40: '40'
+        }
+    }
+})
--- tailwind-css/day7/tailwind.config.js
+++ tailwind-css/day7/tailwind.config.js
@@ -1,8 +1,54 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  prefix: '',
+  important: true,
+  content: [
+    "./src/**/*.{html,js}",
+    './index.html'
+  ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: '450px',
+      },
+      colors: {
+        'primary': '#1fb6ff',
+        'primary-dark': '#1fb612',
+        'secondary': '#7e5bef',
+        'danger': '#ff49db',
+        'success': '#ff7849',
+      },
+      spacing: {
+        '4.5': '18px',
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    },
   },
-  plugins: [],
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        '.content-auto': {
+          'content-visibility': 'hidden',
+        },
+        '.content-hidden': {
+          'content-visibility': 'hidden',
+        },
+        '.content-visible': {
+          'content-visibility': 'visible',
+        },
+      })
+    }),
+    require('./src/plugins/tab-size')
+  ],
 }
